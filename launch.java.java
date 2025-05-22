8c7aff86e119f5fcc6314abca3ed3289634dfce1
Rahul Chouhan <rahulc262002@gmail.com>
2025-05-22 17:33:42 +0530

    modified after cloning

--- launch.java.java
+++ launch.java.java
@@ -3,4 +3,12 @@ class launch
        public static void main(string[] args)
        {
                system.out.println("Exploring Git & GithHub!");
+       }
+
+       public static void fun()
+       {
+               int a = 10;
+               int b = 20;
+               inr sum.out.println(sum);
+       }
 }
